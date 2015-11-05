73e649677f8021662225f236fdcad31005e7c302
Ethan Chang <ejc24@duke.edu>
2015-11-05 17:50:13 -0500

    initial commit

---
+++ app/js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* App Module */
+
+var donationsApp = angular.module('donationsApp', [
+  'ngRoute',
+  'phonecatAnimations',
+
+  'donationsControllers',
+  'phonecatFilters',
+  'phonecatServices'
+]);
+
+donationsApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'HomeCtrl'
+      }).
+      when('/member/:memberId', {
+        templateUrl: 'partials/member-detail.html',
+        controller: 'MemberCtrl'
+      }).
+      when('/donor/:donorId', {
+        templateUrl: 'partials/donor-detail.html',
+        controller: 'DonorCtrl'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }]);
---
+++ app/js/controllers.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/* Controllers */
+
+var donationsControllers = angular.module('donationsControllers', []);
+
+
+donationsControllers.controller('HomeCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('http://localhost:8080/congress').success(function(data) {
+      $scope.members = data;
+    }).error(function(){
+      alert("Error");
+    });
+  }]);
+
+
+donationsControllers.controller('MemberCtrl', ['$scope', '$http', '$routeParams',
+  function($scope, $http, $routeParams) {
+    $http.get('http://localhost:8080/member/' + $routeParams.memberId ).success(function(data) {
+      $scope.member = data;
+    }).error(function(){
+      alert("Error");
+    });
+  }]);
+
+donationsControllers.controller('DonorCtrl', ['$scope', '$http', '$routeParams',
+  function($scope, $http, $routeParams) {
+    $http.get('http://localhost:8080/contributor/' + $routeParams.donorId ).success(function(data) {
+      $scope.donor = data;
+    }).error(function(){
+      alert("Error");
+    });
+  }]);
